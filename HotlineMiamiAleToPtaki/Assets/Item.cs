e23f93df14339a5a26badcbc4738f6c6802c0f51
Jan Krzyszkowski <jan.krzyszkowski2@gmail.com>
2025-02-28 21:36:16 +0100

    grab and throwing

---
+++ HotlineMiamiAleToPtaki/Assets/Item.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName;
+
+    private void Awake()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity *= 5f;
+            rb.linearDamping = 5f;
+        }
+    }
+}
